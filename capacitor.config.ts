9fcc8ff373e2a0d33341e272d376c00c12cbcc3e
HilfaRaisya496 <hilfaraisya01@gmail.com>
2026-05-08 15:12:09 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Zeven',
+  webDir: 'www'
+};
+
+export default config;
